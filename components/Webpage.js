374a344db1483cd364ba2dc1902a6ea0c27ec421
github username <github email address>
2017-06-05 15:15:43 -0400

    done

---
+++ components/Webpage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {FillerText} from './fillerText'
+
+class Webpage extends React.Component {
+    render() {
+        return (
+            <div>
+                <title>The worlds coolest webpage</title>
+                <FillerText />
+                <FillerText />
+            </div>
+        )
+    }
+}
